079c6845e0cba25b66aa869c745231d8c69d0502
Davor Tvoric <davor.tvoric@gmail.com>
2018-03-04 19:31:04 +0100

    Add LESS and some basic styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 let gulp = require('gulp');
 let browserSync = require('browser-sync').create();
+let less = require('gulp-less');
 
+gulp.task('less', function(){
+    return gulp.src('./Applications/Notemaster/style.less')
+    .pipe(less())
+    .pipe(gulp.dest('./Applications/Notemaster'))
+    .pipe(browserSync.reload({
+        stream: true
+    }))
+});
 
 gulp.task('serve', function(){
     browserSync.init({
@@ -8,6 +17,6 @@ gulp.task('serve', function(){
             baseDir: 'Applications/Notemaster'
         }
     })
-
-    gulp.watch("./Applications/Notemaster/*.*").on("change", browserSync.reload);
+    gulp.watch("./Applications/Notemaster/*.less", ['less']);
+    gulp.watch("./Applications/Notemaster/*.index").on("change", browserSync.reload);
 });
