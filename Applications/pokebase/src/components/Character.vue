b88309dee7579db47ee91cca0e04e3bffa5708f4
Davor Tvoric <davor.tvoric@gmail.com>
2018-03-19 21:52:12 +0100

    Implement navigating through pokemon history

--- Applications/pokebase/src/components/Character.vue
+++ Applications/pokebase/src/components/Character.vue
@@ -1,17 +1,20 @@
 <template>
 <div class="wrapper">
-  <div v-bind:style="{ display: isLoading ? 'none': '' }" class="single-char"
-       @click="switchPokemon">
-    <h4 v-if="character.name" class="name">{{capitalize(character.name)}}</h4>
-    <img v-if="character.sprites" class="image" v-bind:src="character.sprites.front_default">
-    <div class="types"><span v-for="(type, index) in character.types" :key="index" class="type">{{type.type.name}}</span></div>
+  <div v-bind:style="{ display: isLoading ? 'none': '', 
+  cursor: this.lastPokemonIndex === this.pokemonHistory.length - 1 ? 'pointer' : 'default' }" class="single-char"
+       @click="switchPokemon" >
+    <h4 v-if="pokemon.name" class="name">{{capitalize(pokemon.name)}}</h4>
+    <img v-if="pokemon.sprites" class="image" v-bind:src="pokemon.sprites.front_default">
+    <div class="types"><span v-for="(type, index) in pokemon.types" :key="index" class="type">{{type.type.name}}</span></div>
     <h5 class="hp-label">HP</h5>
-    <h5 class="hp-value" v-for="(stat, index) in character.stats" :key="index" v-if="stat.stat.name === 'hp'">{{stat.base_stat}}</h5>
+    <h5 class="hp-value" v-for="(stat, index) in pokemon.stats" :key="index" v-if="stat.stat.name === 'hp'">{{stat.base_stat}}</h5>
     <h5 class="weight-label">Weight</h5>
-    <h5 class="weight-value">{{character.weight}}</h5>
+    <h5 class="weight-value">{{pokemon.weight}}</h5>
     <h5 class="height-label">Height</h5>
-    <h5 class="height-value">{{character.height}}</h5>
+    <h5 class="height-value">{{pokemon.height}}</h5>
   </div>
+  <button v-bind:style="{ display: (lastPokemonIndex === 0 || isLoading) ? 'none': '' }" @click="setPreviousPokemon" class="nav-btn left">Back</button>
+  <button v-bind:style="{ display: (lastPokemonIndex === pokemonHistory.length -1 || isLoading) ? 'none': '' }" @click="setNextPokemon" class="nav-btn right">Forwards</button>
   <div class="loader" v-bind:style="{ display: !isLoading ? 'none': '' }">{{'Loading' + dots}}</div>
 </div>
 </template>
@@ -21,33 +24,49 @@ export default {
   props: ["id"],
   data() {
     return {
-      character: {},
+      pokemon: {},
       isLoading: true,
-      dots: ''
+      dots: "",
+      pokemonHistory: [],
+      lastPokemonIndex: 0
     };
   },
   methods: {
     fetchPokemon(id) {
       this.isLoading = true;
-      let interval = setInterval(()=>{
-        if(this.dots.length>2) {this.dots = ''; return; };
-        this.dots += '.';
-      }, 1000)
+      let interval = setInterval(() => {
+        if (this.dots.length > 2) {
+          this.dots = "";
+          return;
+        }
+        this.dots += ".";
+      }, 1000);
       fetch("https://pokeapi.co/api/v2/pokemon/" + id + "/", {
         method: "GET"
       }).then((response, error) => {
         clearInterval(interval);
         this.isLoading = false;
         response.json().then(json => {
-          this.character = json;
+          this.pokemon = json;
+          this.pokemonHistory.push(this.pokemon);
+          this.lastPokemonIndex = this.pokemonHistory.length - 1;
         });
       });
     },
     switchPokemon() {
-      this.fetchPokemon(Math.floor(Math.random() * 151) + 1);
+      if (this.lastPokemonIndex === this.pokemonHistory.length - 1)
+        this.fetchPokemon(Math.floor(Math.random() * 151) + 1);
     },
     capitalize(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
+    },
+    setPreviousPokemon() {
+      this.lastPokemonIndex = --this.lastPokemonIndex;
+      this.pokemon = this.pokemonHistory[this.lastPokemonIndex];
+    },
+    setNextPokemon() {
+      this.lastPokemonIndex = ++this.lastPokemonIndex;
+      this.pokemon = this.pokemonHistory[this.lastPokemonIndex];
     }
   },
   created() {
@@ -68,7 +87,7 @@ export default {
   text-align: center;
 }
 
-.loader{
+.loader {
   text-align: center;
   height: 100%;
   transform: translateY(50%);
@@ -81,9 +100,26 @@ export default {
   color: #1d3557;
 }
 
-.wrapper{
- width: calc(25% - 10px);
- height: 250px;
+.wrapper {
+  width: calc(25% - 10px);
+  height: 250px;
+  .nav-btn {
+    border: 2px solid #457b9d;
+    background: #a8dadc;
+    padding: 8px 24px;
+    font-weight: bold;
+    margin-top: 5px;
+    font-size: 1.2em;
+    text-transform: uppercase;
+    color: #457b9d;
+    cursor: pointer;
+  }
+  .left {
+    float: left;
+  }
+  .right {
+    float: right;
+  }
 }
 
 .single-char {
@@ -92,7 +128,6 @@ export default {
   background: #e63946;
   border: 2px solid #1d3557;
   border-radius: 11px;
-  cursor: pointer;
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 25px 93px 36px 30px 30px 30px;
